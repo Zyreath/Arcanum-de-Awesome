4c885254a0eb482fc686eb12e356873637b21ee3
Cody <zerra_26@yahoo.com>
2023-05-28 06:23:53 -0400

    Update first_javascript.js

--- projects/first_javascript.js
+++ projects/first_javascript.js
@@ -16,9 +16,10 @@ var l = [];
 for(var i = 0; i < 101; i++){
   if (i%3 == 0) {
    l.push("Fizz");
+  } else if (i%5 == 0) {
+   l.push("Buzz");
   } else {
-   l.push(i);
-  }
+ l.push(i);
+   }
 }
-
 arrayOutputElement.textContent = l.join(", ");
