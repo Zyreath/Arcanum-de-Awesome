1534b7f4eb1ec6052306ddf8d4d239da4b17ee50
Cody <zerra_26@yahoo.com>
2023-05-28 05:18:21 -0400

    Update first_javascript.js

--- projects/first_javascript.js
+++ projects/first_javascript.js
@@ -7,3 +7,10 @@ for (var i = 0; i < 7; i++) {
 }
 
 document.getElementById("ex1").innerHTML = ans;
+
+var l = [];
+for(var i = 0; i < 30; i++){
+   l.append(i);
+}
+
+document.getElementById("ex2").innerHTML = l;
