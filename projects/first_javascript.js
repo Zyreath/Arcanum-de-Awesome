e3798ac38c618f23acb94b402c53025dbd8fad29
Cody <zerra_26@yahoo.com>
2023-05-28 05:56:09 -0400

    Update first_javascript.js

--- projects/first_javascript.js
+++ projects/first_javascript.js
@@ -13,7 +13,7 @@ document.getElementById("ex1").innerHTML = ans;
 var arrayOutputElement = document.getElementById("ex2");
 
 var l = [];
-for(var i = 0; i < 30; i++){
+for(var i = 0; i <= 100; i++){
    l.push(i);
 }
 
