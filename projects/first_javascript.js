8bbfe3c0a506dd6ad6a348a4a8c5e6fa67eae1f2
Cody <zerra_26@yahoo.com>
2023-05-28 04:05:56 -0400

    Update first_javascript.js

--- projects/first_javascript.js
+++ projects/first_javascript.js
@@ -1,8 +1,8 @@
-let ans = document.getElementById("ex1").innerHTML;
-ans = "";
+var ans = document.getElementById("ex1").innerHTML;
 
 var s = "#";
 for (var i = 0; i < 7; i++){
   ans += s;
   s += "#";
+  console.log(s);
 }
