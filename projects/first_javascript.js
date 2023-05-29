2d204e7d98417341f5b756eb8444610876661763
Cody <zerra_26@yahoo.com>
2023-05-29 00:48:19 -0400

    Update first_javascript.js

--- projects/first_javascript.js
+++ projects/first_javascript.js
@@ -41,7 +41,9 @@ for (var i = 0; i < 65; i++) {
   if (pattern.length % 8 == 0) {
     pattern += '\n';
   } 
+  console.log(i)
 }
 
 document.getElementById("ex3").innerHTML = pattern;
 console.log(pattern);
+console.log(counter)
